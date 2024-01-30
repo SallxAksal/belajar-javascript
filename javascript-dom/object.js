6e22055927da6ebca8b3ba9c7e5726cba03f6466
githubna-aksal <triansyahaksal.com>
2024-01-30 08:39:50 +0700

    dom

--- javascript-dom/object.js
+++ javascript-dom/object.js
@@ -3,5 +3,11 @@ siswa.namaDepan = "Akzal";
 siswa.namaBelakang = "Damusti";
 siswa.alamat = "bandung";
 siswa.namaLengkap = function() {
-    
-}
+    return this.namaDepan + "" + this.namaBelakang;
+};
+
+alert("Nama :" + siswa.namaLengkap());
+
+//function namaLengkap() {
+
+// //}
