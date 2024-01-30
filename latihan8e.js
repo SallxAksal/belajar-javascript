9fbd732a8e3b080e0daa9cd1f421e252283db872
githubna-aksal <triansyahaksal.com>
2024-01-30 07:35:10 +0700

    latihan8e

--- latihan8e.js
+++ latihan8e.js
@@ -2,11 +2,11 @@ var jumlahBaris = prompt("Masukkan jumlah baris:");
 jumlahBaris = parseInt(jumlahBaris);
 var hasilBaris = '';
 
-for (var i = 1; i <= jumlahBaris; i++) {
+for (var i = 0; i <= jumlahBaris; i++) {
   for (var j = 1; j <= jumlahBaris - i; j++) {
     hasilBaris += '+';
   }
-  for (var k = 1; k <= i; k++) {
+  for (var k = 0; k <= i; k++) {
     hasilBaris += '#';
   }
   hasilBaris += '\n';
